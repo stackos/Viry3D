aaa3fe1d21e972389140bf167ab3c84546e0370c
Stack <stackos@qq.com>
2017-10-26 21:28:58 +0800

    improve some variable name.

--- app/src/AppShadow.cpp
+++ app/src/AppShadow.cpp
@@ -54,9 +54,9 @@ public:
 
 		int shadowmap_size = 1024;
 
-		auto rt = RefMake<FrameBuffer>();
-		rt->color_texture = RenderTexture::Create(shadowmap_size, shadowmap_size, RenderTextureFormat::R8, DepthBuffer::Depth_0, FilterMode::Bilinear);
-		rt->depth_texture = RenderTexture::Create(shadowmap_size, shadowmap_size, RenderTextureFormat::Depth, DepthBuffer::Depth_24, FilterMode::Bilinear);
+		auto rt_shadow = RefMake<FrameBuffer>();
+		rt_shadow->color_texture = RenderTexture::Create(shadowmap_size, shadowmap_size, RenderTextureFormat::R8, DepthBuffer::Depth_0, FilterMode::Bilinear);
+		rt_shadow->depth_texture = RenderTexture::Create(shadowmap_size, shadowmap_size, RenderTextureFormat::Depth, DepthBuffer::Depth_24, FilterMode::Bilinear);
 		
 		auto camera_shadow = GameObject::Create("camera")->AddComponent<Camera>();
 		camera_shadow->SetOrthographic(true);
@@ -66,36 +66,36 @@ public:
 		camera_shadow->SetDepth(-1);
 		camera_shadow->SetCullingMask(1 << 0);
 		camera_shadow->GetTransform()->SetRotation(Quaternion::Euler(45, 145, 0));
-		camera_shadow->SetFrameBuffer(rt);
+		camera_shadow->SetFrameBuffer(rt_shadow);
 		camera_shadow->SetRenderMode(CameraRenderMode::ShadowMap);
 
-		auto obj = Resource::LoadGameObject("Assets/AppAnim/unitychan.prefab");
-		obj->GetTransform()->SetRotation(Quaternion::Euler(0, 180, 0));
-		auto anim = obj->GetComponent<Animation>();
-		auto state = anim->GetAnimationState("WAIT03");
-		state.wrap_mode = AnimationWrapMode::Loop;
-		anim->UpdateAnimationState("WAIT03", state);
+		auto anim_obj = Resource::LoadGameObject("Assets/AppAnim/unitychan.prefab");
+		anim_obj->GetTransform()->SetRotation(Quaternion::Euler(0, 180, 0));
+		auto anim = anim_obj->GetComponent<Animation>();
+		auto anim_state = anim->GetAnimationState("WAIT03");
+		anim_state.wrap_mode = AnimationWrapMode::Loop;
+		anim->UpdateAnimationState("WAIT03", anim_state);
 		anim->Play("WAIT03");
 
-		auto mesh = Resource::LoadMesh("Assets/Library/unity default resources.Plane.mesh");
-		mesh->Update();
+		auto plane_mesh = Resource::LoadMesh("Assets/Library/unity default resources.Plane.mesh");
+		plane_mesh->Update();
 
 		float shadow_bias = 0.005f;
 		float shadow_strength = 0.7f;
 
-		auto mat = Material::Create("Shadow/Diffuse");
-		mat->SetTexture("_ShadowMap", rt->depth_texture);
-		mat->SetMatrix("_ViewProjectionLight", camera_shadow->GetViewProjectionMatrix());
-		mat->SetVector("_ShadowMapTexel", Vector4(1.0f / shadowmap_size, 1.0f / shadowmap_size));
-		mat->SetVector("_ShadowParam", Vector4(shadow_bias, shadow_strength));
+		auto ground_mat = Material::Create("Shadow/Diffuse");
+		ground_mat->SetTexture("_ShadowMap", rt_shadow->depth_texture);
+		ground_mat->SetMatrix("_ViewProjectionLight", camera_shadow->GetViewProjectionMatrix());
+		ground_mat->SetVector("_ShadowMapTexel", Vector4(1.0f / shadowmap_size, 1.0f / shadowmap_size));
+		ground_mat->SetVector("_ShadowParam", Vector4(shadow_bias, shadow_strength));
 
 		auto ground = GameObject::Create("ground")->AddComponent<MeshRenderer>();
-		ground->SetSharedMaterial(mat);
-		ground->SetSharedMesh(mesh);
+		ground->SetSharedMaterial(ground_mat);
+		ground->SetSharedMesh(plane_mesh);
 
 		camera->SetPostRenderFunc([=]() {
 			Viry3D::Rect rect(0.8f, 0.8f, 0.2f, 0.2f);
-			Graphics::DrawQuad(&rect, rt->depth_texture, true);
+			Graphics::DrawQuad(&rect, rt_shadow->depth_texture, true);
 		});
 
 		//Graphics::GetDisplay()->BeginRecord("../../../demo.mp4");
