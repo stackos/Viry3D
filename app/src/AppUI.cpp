78c90a4a24cbf52d731e68427150d0226dc5fe8f
Stack <stackos@qq.com>
2017-11-18 17:26:36 +0800

    window fade in.

--- app/src/AppUI.cpp
+++ app/src/AppUI.cpp
@@ -23,6 +23,7 @@
 #include "ui/UISprite.h"
 #include "ui/UICanvasRenderer.h"
 #include "tweener/TweenUIColor.h"
+#include "tweener/TweenPosition.h"
 
 using namespace Viry3D;
 
@@ -48,12 +49,15 @@ public:
 		ui->GetTransform()->SetPosition(Vector3::Zero());
 		ui->GetTransform()->SetScale(Vector3::One());
 
-		auto button_main = ui->GetTransform()->Find("button main/border")->GetGameObject()->GetComponent<UISprite>();
-		button_main->event_handler.enable = true;
-		button_main->event_handler.on_pointer_click = [=](UIPointerEvent& e) {
-			auto window_menu = ui->GetTransform()->Find("window menu")->GetGameObject();
+		auto window_menu = ui->GetTransform()->Find("window menu")->GetGameObject();
+		m_window_menu_pos = window_menu->GetTransform()->GetLocalPosition();
+
+		auto button_main_border = ui->GetTransform()->Find("button main/border")->GetGameObject()->GetComponent<UISprite>();
+		button_main_border->event_handler.enable = true;
+		button_main_border->event_handler.on_pointer_click = [=](UIPointerEvent& e) {
 			window_menu->SetActive(true);
 
+			// tween color in
 			auto renderers = window_menu->GetComponentsInChildren<UICanvasRenderer>();
 			for (auto& i : renderers)
 			{
@@ -64,8 +68,24 @@ public:
 			tc->duration = 0.2f;
 			tc->from = Color(1, 1, 1, 0);
 			tc->to = Color(1, 1, 1, 1);
+
+			// tween pos in
+			auto start_pos = m_window_menu_pos + Vector3(-10, 0, 0);
+			window_menu->GetTransform()->SetLocalPosition(start_pos);
+			auto tp = window_menu->AddComponent<TweenPosition>();
+			tp->duration = 0.2f;
+			tp->from = start_pos;
+			tp->to = m_window_menu_pos;
+		};
+
+		auto window_menu_border = ui->GetTransform()->Find("window menu/border")->GetGameObject()->GetComponent<UISprite>();
+		window_menu_border->event_handler.enable = true;
+		window_menu_border->event_handler.on_pointer_click = [=](UIPointerEvent& e) {
+			window_menu->SetActive(false);
 		};
     }
+
+	Vector3 m_window_menu_pos;
 };
 
 #if 1
