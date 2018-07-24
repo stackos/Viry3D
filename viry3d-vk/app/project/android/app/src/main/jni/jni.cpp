f689cd7f109c3003b3f3d5c7b2e9b97e85487239
Stack <stackos@qq.com>
2018-07-24 21:11:01 +0800

    add android input.

--- viry3d-vk/app/project/android/app/src/main/jni/jni.cpp
+++ viry3d-vk/app/project/android/app/src/main/jni/jni.cpp
@@ -18,9 +18,11 @@
 #include <android_native_app_glue.h>
 #include "Application.h"
 #include "Debug.h"
+#include "Input.h"
 #include "io/File.h"
 #include "io/Directory.h"
 #include "container/List.h"
+#include "container/FastList.h"
 #include "thread/ThreadPool.h"
 #include "graphics/Display.h"
 #include "App.h"
@@ -31,11 +33,34 @@ static void extract_assets_if_needed(const String& package_path, const String& d
 static int call_activity_method_int(ANativeActivity* activity, const char* name, const char* sig, ...);
 static String call_activity_method_string(ANativeActivity* activity, const char* name, const char* sig, ...);
 
+struct TouchEvent
+{
+    int act;
+    int index;
+    int id;
+    int count;
+    long long time;
+    float xys[20];
+};
+
+extern Vector<Touch> g_input_touches;
+extern List<Touch> g_input_touch_buffer;
+extern bool g_key_down[(int) KeyCode::COUNT];
+extern bool g_key[(int) KeyCode::COUNT];
+extern bool g_key_up[(int) KeyCode::COUNT];
+extern bool g_mouse_button_down[3];
+extern bool g_mouse_button_up[3];
+extern Vector3 g_mouse_position;
+extern bool g_mouse_button_held[3];
+
 static android_app* g_android_app = nullptr;
-static bool g_display_init = false;
 static bool g_can_draw = false;
 static Display* g_display = nullptr;
+static int g_display_width = 0;
+static int g_display_height = 0;
 static App* g_app = nullptr;
+static FastList<Event> g_events;
+static Mutex g_mutex;
 
 static void engine_create()
 {
@@ -51,12 +76,12 @@ static void engine_create()
 	extract_assets_if_needed(package_path, data_path, true);
 
     String name = "viry3d-vk-demo";
-    int width = ANativeWindow_getWidth(g_android_app->window);
-    int height = ANativeWindow_getHeight(g_android_app->window);
+    g_display_width = ANativeWindow_getWidth(g_android_app->window);
+    g_display_height = ANativeWindow_getHeight(g_android_app->window);
 
-    Log("init display with window width: %d height: %d", width, height);
+    Log("init display width: %d height: %d", g_display_width, g_display_height);
 
-    g_display = new Display(name, g_android_app->window, width, height);
+    g_display = new Display(name, g_android_app->window, g_display_width, g_display_height);
 
     g_app = new App();
     g_app->SetName(name);
@@ -100,6 +125,238 @@ static void draw()
     g_app->OnFrameEnd();
 }
 
+static int get_key(int key_code)
+{
+    int key = -1;
+
+    if(key_code == 82)//KEYCODE_MENU
+    {
+        key = (int) KeyCode::Menu;
+    }
+    else if(key_code == 4)//KEYCODE_BACK
+    {
+        key = (int) KeyCode::Backspace;
+    }
+    else if(key_code == 24)//KEYCODE_VOLUME_UP
+    {
+        key = (int) KeyCode::PageUp;
+    }
+    else if(key_code == 25)//KEYCODE_VOLUME_DOWN
+    {
+        key = (int) KeyCode::PageDown;
+    }
+
+    return key;
+}
+
+static void on_key_down(int key_code)
+{
+    int key = get_key(key_code);
+    if (key >= 0)
+    {
+        if (!g_key[key])
+        {
+            g_key_down[key] = true;
+            g_key[key] = true;
+        }
+    }
+}
+
+static void on_key_up(int key_code)
+{
+    int key = get_key(key_code);
+    if (key >= 0)
+    {
+        g_key_up[key] = true;
+        g_key[key] = false;
+    }
+}
+
+static void touch_begin(const void* event)
+{
+    const TouchEvent* t = (const TouchEvent*) event;
+
+    float x = t->xys[t->index * 2];
+    float y = t->xys[t->index * 2 + 1];
+
+    Touch touch;
+    touch.deltaPosition = Vector2(0, 0);
+    touch.deltaTime = 0;
+    touch.time = t->time / 1000.0f;
+    touch.fingerId = t->id;
+    touch.phase = TouchPhase::Began;
+    touch.tapCount = t->count;
+    touch.position = Vector2(x, y);
+
+    if (!g_input_touches.Empty())
+    {
+        g_input_touch_buffer.AddLast(touch);
+    }
+    else
+    {
+        g_input_touches.Add(touch);
+    }
+
+    if (touch.fingerId == 0)
+    {
+        g_mouse_button_down[0] = true;
+        g_mouse_position.x = x;
+        g_mouse_position.y = y;
+        g_mouse_button_held[0] = true;
+    }
+}
+
+static void touch_update(const void* event)
+{
+    const TouchEvent* t = (const TouchEvent*) event;
+
+    float x = t->xys[t->index * 2];
+    float y = t->xys[t->index * 2 + 1];
+
+    Touch touch;
+    touch.deltaPosition = Vector2(0, 0);
+    touch.deltaTime = 0;
+    touch.time = t->time / 1000.0f;
+    touch.fingerId = t->id;
+    touch.tapCount = t->count;
+    touch.position = Vector2(x, y);
+
+    if (t->act == AMOTION_EVENT_ACTION_MOVE)
+    {
+        touch.phase = TouchPhase::Moved;
+    }
+    else
+    {
+        touch.phase = TouchPhase::Ended;
+    }
+
+    if (!g_input_touches.Empty())
+    {
+        g_input_touch_buffer.AddLast(touch);
+    }
+    else
+    {
+        g_input_touches.Add(touch);
+    }
+
+    if (touch.fingerId == 0)
+    {
+        if (touch.phase == TouchPhase::Ended)
+        {
+            g_mouse_button_up[0] = true;
+            g_mouse_position.x = x;
+            g_mouse_position.y = y;
+            g_mouse_button_held[0] = false;
+        }
+        else if (touch.phase == TouchPhase::Moved)
+        {
+            g_mouse_position.x = x;
+            g_mouse_position.y = y;
+        }
+    }
+}
+
+static void on_touch(const TouchEvent& touch)
+{
+    switch (touch.act)
+    {
+        case AMOTION_EVENT_ACTION_DOWN:
+        case AMOTION_EVENT_ACTION_POINTER_DOWN:
+            touch_begin(&touch);
+            break;
+
+        case AMOTION_EVENT_ACTION_MOVE:
+        case AMOTION_EVENT_ACTION_UP:
+        case AMOTION_EVENT_ACTION_POINTER_UP:
+        case AMOTION_EVENT_ACTION_CANCEL:
+            touch_update(&touch);
+            break;
+
+        default:
+            break;
+    }
+}
+
+static void process_events()
+{
+    g_mutex.lock();
+
+    for (auto i : g_events)
+    {
+        if (i)
+        {
+            i();
+        }
+    }
+    g_events.Clear();
+
+    g_mutex.unlock();
+}
+
+static void queue_event(Event event)
+{
+    g_mutex.lock();
+
+    g_events.AddLast(event);
+
+    g_mutex.unlock();
+}
+
+static int32_t handle_input(struct android_app*, AInputEvent* event)
+{
+    auto type = AInputEvent_getType(event);
+    if (type == AINPUT_EVENT_TYPE_KEY)
+    {
+        auto action = AKeyEvent_getAction(event);
+        auto code = AKeyEvent_getKeyCode(event);
+
+        if (action == AKEY_EVENT_ACTION_DOWN)
+        {
+            queue_event([=]() {
+                on_key_down(code);
+            });
+        }
+        else if(action == AKEY_EVENT_ACTION_UP)
+        {
+            queue_event([=]() {
+                on_key_up(code);
+            });
+
+            return 1;
+        }
+    }
+    else if (type == AINPUT_EVENT_TYPE_MOTION)
+    {
+        int action = AMotionEvent_getAction(event);
+        int count = (int) AMotionEvent_getPointerCount(event);
+        int index = (action & 0xff00) >> 8;
+        int id = AMotionEvent_getPointerId(event, (size_t) index);
+        long long time = AMotionEvent_getEventTime(event);
+        int act = action & 0xff;
+
+        if (count <= 10)
+        {
+            TouchEvent touch;
+            touch.act = act;
+            touch.index = index;
+            touch.id = id;
+            touch.count = count;
+            touch.time = time;
+            for (size_t i = 0; i < count; i++)
+            {
+                touch.xys[i * 2] = AMotionEvent_getX(event, i);
+                touch.xys[i * 2 + 1] = (float) g_display_height - AMotionEvent_getY(event, i) - 1;
+            }
+
+            queue_event([=](){
+                on_touch(touch);
+            });
+        }
+    }
+
+    return 0;
+}
+
 enum class APP_CMD {
     APP_CMD_INPUT_CHANGED,
     APP_CMD_INIT_WINDOW,
@@ -125,40 +382,46 @@ static void handle_cmd(android_app* app, int32_t cmdi)
 
 	switch (cmd)
 	{
-        case APP_CMD::APP_CMD_GAINED_FOCUS:
-            if (!g_display_init)
-            {
-                g_display_init = true;
-
-                engine_create();
-            }
-            else
-            {
-                engine_resume();
-            }
+        case APP_CMD::APP_CMD_START:
+            Log("APP_CMD_START");
             break;
 
-        case APP_CMD::APP_CMD_LOST_FOCUS:
-            engine_pause();
+        case APP_CMD::APP_CMD_STOP:
+            Log("APP_CMD_STOP");
+            break;
+
+        case APP_CMD::APP_CMD_DESTROY:
+            Log("APP_CMD_DESTROY");
             break;
 
         case APP_CMD::APP_CMD_INIT_WINDOW:
-            // set keep screen on will recreate window, so recreate surface
-            if (g_display_init && g_can_draw)
+            Log("APP_CMD_INIT_WINDOW");
+            if (g_display)
             {
-                engine_pause();
-
                 engine_resume();
+            }
+            else
+            {
+                engine_create();
             }
             break;
 
-		case APP_CMD::APP_CMD_CONFIG_CHANGED:
-            if (g_can_draw && app->window)
-            {
-                //int w = ANativeWindow_getWidth(app->window);
-                //int h = ANativeWindow_getHeight(app->window);
-            }
-		    break;
+        case APP_CMD::APP_CMD_TERM_WINDOW:
+            Log("APP_CMD_TERM_WINDOW");
+            break;
+
+        case APP_CMD::APP_CMD_WINDOW_RESIZED:
+            Log("APP_CMD_WINDOW_RESIZED");
+            break;
+
+        case APP_CMD::APP_CMD_PAUSE:
+            Log("APP_CMD_PAUSE");
+            engine_pause();
+            break;
+
+        case APP_CMD::APP_CMD_RESUME:
+            Log("APP_CMD_RESUME");
+            break;
 
 		default:
 			break;
@@ -170,10 +433,9 @@ void android_main(android_app* app)
 	Log("android_main begin");
 
 	app->onAppCmd = handle_cmd;
-	//app->onInputEvent = handle_input;
+	app->onInputEvent = handle_input;
 
     g_android_app = app;
-    g_display_init = false;
     g_can_draw = false;
 
 	int events;
@@ -194,7 +456,7 @@ void android_main(android_app* app)
 			break;
 		}
 
-        //process_events();
+        process_events();
 
 		if (g_can_draw)
 		{
@@ -253,7 +515,7 @@ static String call_activity_method_string(ANativeActivity* activity, const char*
 	return result;
 }
 
-static void extract_assets(const String& source, const String& dest, bool directory)
+static void extract_assets(const String& source, const String& dest)
 {
     auto mgr = g_android_app->activity->assetManager;
     Vector<String> files;
@@ -329,7 +591,7 @@ static void extract_assets_if_needed(const String& package_path, const String& d
         // i don't know why,
         // so use asset manager instead.
         //File::Unzip(package_path, "assets/Assets", data_path, true);
-        extract_assets("Assets", data_path, true);
+        extract_assets("Assets", data_path);
 	}
 	else
 	{
@@ -344,7 +606,7 @@ void java_keep_screen_on(bool enable)
 
 void java_quit_application()
 {
-	call_activity_method_int(g_android_app->activity, "quitApplication", "()I");
+    ANativeActivity_finish(g_android_app->activity);
 }
 
 void* get_native_window()
