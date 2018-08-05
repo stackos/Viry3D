63d3f4e0d7adc55d8892cf96fa5e62a86f2e7063
stackos <stackos@qq.com>
2018-08-06 00:22:09 +0800

    sample shadow with poisson filter.

--- app/src/DemoShadowMap.h
+++ app/src/DemoShadowMap.h
@@ -19,9 +19,11 @@
 
 #include "DemoMesh.h"
 
+#define SHADOW_MAP_SIZE 1024
+
 namespace Viry3D
 {
-    class DemoShadowMap : public DemoMesh
+    class DemoShadowMap: public DemoMesh
     {
     public:
         struct LightParam
@@ -53,16 +55,15 @@ namespace Viry3D
         {
             m_camera->SetDepth(1);
 
-            const int shadow_map_size = 1024;
             m_shadow_texture = Texture::CreateRenderTexture(
-                    shadow_map_size,
-                    shadow_map_size,
-                    Display::Instance()->ChooseFormatSupported(
-                            { VK_FORMAT_D32_SFLOAT, VK_FORMAT_X8_D24_UNORM_PACK32, VK_FORMAT_D32_SFLOAT_S8_UINT, VK_FORMAT_D24_UNORM_S8_UINT },
-                            VK_FORMAT_FEATURE_DEPTH_STENCIL_ATTACHMENT_BIT | VK_FORMAT_FEATURE_SAMPLED_IMAGE_BIT),
-                    true,
-                    VK_FILTER_LINEAR,
-                    VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_EDGE);
+                SHADOW_MAP_SIZE,
+                SHADOW_MAP_SIZE,
+                Display::Instance()->ChooseFormatSupported(
+                    { VK_FORMAT_D32_SFLOAT, VK_FORMAT_X8_D24_UNORM_PACK32, VK_FORMAT_D32_SFLOAT_S8_UINT, VK_FORMAT_D24_UNORM_S8_UINT },
+                    VK_FORMAT_FEATURE_DEPTH_STENCIL_ATTACHMENT_BIT | VK_FORMAT_FEATURE_SAMPLED_IMAGE_BIT),
+                true,
+                VK_FILTER_LINEAR,
+                VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_EDGE);
 
             m_shadow_camera = Display::Instance()->CreateCamera();
             m_shadow_camera->SetDepth(0);
@@ -101,21 +102,21 @@ void main()
             render_state.cull = RenderState::Cull::Front;
 
             auto shader = RefMake<Shader>(
-                    "",
-                    Vector<String>(),
-                    vs,
-                    "",
-                    Vector<String>(),
-                    fs,
-                    render_state);
+                "",
+                Vector<String>(),
+                vs,
+                "",
+                Vector<String>(),
+                fs,
+                render_state);
             auto material = RefMake<Material>(shader);
 
             Vector3 light_forward = m_light_param.rot * Vector3(0, 0, 1);
             Vector3 light_up = m_light_param.rot * Vector3(0, 1, 0);
             Matrix4x4 view = Matrix4x4::LookTo(m_light_param.pos, light_forward, light_up);
 
-            int target_width = shadow_map_size;
-            int target_height = shadow_map_size;
+            int target_width = SHADOW_MAP_SIZE;
+            int target_height = SHADOW_MAP_SIZE;
             float ortho_size = m_light_param.ortho_size;
             float top = ortho_size;
             float bottom = -ortho_size;
@@ -189,7 +190,7 @@ UniformBuffer(0, 2) uniform UniformBuffer02
     float u_light_intensity;
     float u_shadow_strength;
     float u_shadow_z_bias;
-    float u_shadow_uv_step;
+    float u_shadow_filter_radius;
 } buf_0_2;
 
 Input(0) vec2 v_uv;
@@ -197,6 +198,34 @@ Input(1) vec3 v_normal;
 Input(2) vec4 v_pos_light_proj;
 
 Output(0) vec4 o_frag;
+
+const vec2 Poisson25[25] = vec2[](
+    vec2(-0.978698, -0.0884121),
+    vec2(-0.841121, 0.521165),
+    vec2(-0.71746, -0.50322),
+    vec2(-0.702933, 0.903134),
+    vec2(-0.663198, 0.15482),
+    vec2(-0.495102, -0.232887),
+    vec2(-0.364238, -0.961791),
+    vec2(-0.345866, -0.564379),
+    vec2(-0.325663, 0.64037),
+    vec2(-0.182714, 0.321329),
+    vec2(-0.142613, -0.0227363),
+    vec2(-0.0564287, -0.36729),
+    vec2(-0.0185858, 0.918882),
+    vec2(0.0381787, -0.728996),
+    vec2(0.16599, 0.093112),
+    vec2(0.253639, 0.719535),
+    vec2(0.369549, -0.655019),
+    vec2(0.423627, 0.429975),
+    vec2(0.530747, -0.364971),
+    vec2(0.566027, -0.940489),
+    vec2(0.639332, 0.0284127),
+    vec2(0.652089, 0.669668),
+    vec2(0.773797, 0.345012),
+    vec2(0.968871, 0.840449),
+    vec2(0.991882, -0.657338)
+);
 
 float linear_filter(float z, vec2 uv)
 {
@@ -210,16 +239,13 @@ float linear_filter(float z, vec2 uv)
     }
 }
 
-float pcf_filter(float z, vec2 uv)
+float poisson_filter(float z, vec2 uv, vec2 filter_radius)
 {
     float shadow = 0.0;
-    for (int i = -2; i <= 2; ++i)
+    for (int i = 0; i < 25; ++i)
     {
-        for (int j = -2; j <= 2; ++j)
-        {
-            vec2 offset = vec2(i, j) * buf_0_2.u_shadow_uv_step;
-            shadow += linear_filter(z, uv + offset);
-        }
+        vec2 offset = Poisson25[i] * filter_radius;
+        shadow += linear_filter(z, uv + offset);
     }
     return shadow / 25.0;
 }
@@ -230,7 +256,7 @@ float sample_shadow(vec4 pos_light_proj)
     uv.y = 1.0 - uv.y;
     float z = pos_light_proj.z * 0.5 + 0.5;
 
-    return pcf_filter(z, uv) * buf_0_2.u_shadow_strength;
+    return poisson_filter(z, uv, vec2(buf_0_2.u_shadow_filter_radius));
 }
 
 void main()
@@ -241,7 +267,7 @@ void main()
     
     float nl = max(dot(n, l), 0.0);
     vec3 diff = c.rgb * nl * buf_0_2.u_light_color.rgb * buf_0_2.u_light_intensity;
-    float shadow = sample_shadow(v_pos_light_proj / v_pos_light_proj.w);
+    float shadow = sample_shadow(v_pos_light_proj / v_pos_light_proj.w) * buf_0_2.u_shadow_strength;
 
     c.rgb = diff * (1.0 - shadow);
     c.a = 1.0;
@@ -268,7 +294,7 @@ void main()
                 material->SetMatrix("u_light_view_projection_matrix", m_light_view_projection_matrix);
                 material->SetFloat("u_shadow_strength", 1.0f);
                 material->SetFloat("u_shadow_z_bias", 0.0001f);
-                material->SetFloat("u_shadow_uv_step", 1.0f / m_shadow_texture->GetWidth());
+                material->SetFloat("u_shadow_filter_radius", 1.0f / SHADOW_MAP_SIZE * 3);
             }
         }
 
