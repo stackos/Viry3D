138a9147805df522cbb032d39893ed477dc33387
Stack <stackos@qq.com>
2018-09-07 22:46:32 +0800

    add angle lib and uwp project.

---
+++ app/project/uwp/App.cpp
@@ -0,0 +1,329 @@
+﻿#include "app.h"
+#include <EGL/eglext.h>
+#include <EGL/eglplatform.h>
+#define GL_GLEXT_PROTOTYPES
+#include <GLES2/gl2.h>
+#include <GLES2/gl2ext.h>
+#include <angle_windowsstore.h>
+#include <string>
+#include <memory>
+
+using namespace Windows::ApplicationModel::Core;
+using namespace Windows::ApplicationModel::Activation;
+using namespace Windows::UI::Core;
+using namespace Windows::UI::Input;
+using namespace Windows::Foundation;
+using namespace Windows::Foundation::Collections;
+using namespace Windows::Graphics::Display;
+using namespace Microsoft::WRL;
+using namespace Platform;
+
+using namespace app;
+
+// Implementation of the IFrameworkViewSource interface, necessary to run our app.
+ref class AppSource sealed : Windows::ApplicationModel::Core::IFrameworkViewSource
+{
+public:
+    virtual Windows::ApplicationModel::Core::IFrameworkView^ CreateView()
+    {
+        return ref new App();
+    }
+};
+
+// The main function creates an IFrameworkViewSource for our app, and runs the app.
+[Platform::MTAThread]
+int main(Platform::Array<Platform::String^>^)
+{
+    auto app_source = ref new AppSource();
+    CoreApplication::Run(app_source);
+    return 0;
+}
+
+App::App():
+    m_window_closed(false),
+    m_window_visible(true),
+    m_display(EGL_NO_DISPLAY),
+    m_context(EGL_NO_CONTEXT),
+    m_surface(EGL_NO_SURFACE)
+{
+}
+
+// The first method called when the IFrameworkView is being created.
+void App::Initialize(CoreApplicationView^ applicationView)
+{
+    // Register event handlers for app lifecycle. This example includes Activated, so that we
+    // can make the CoreWindow active and start rendering on the window.
+    applicationView->Activated += 
+        ref new TypedEventHandler<CoreApplicationView^, IActivatedEventArgs^>(this, &App::OnActivated);
+
+    // Logic for other event handlers could go here.
+    // Information about the Suspending and Resuming event handlers can be found here:
+    // http://msdn.microsoft.com/en-us/library/windows/apps/xaml/hh994930.aspx
+}
+
+// Called when the CoreWindow object is created (or re-created).
+void App::SetWindow(CoreWindow^ window)
+{
+    window->VisibilityChanged +=
+        ref new TypedEventHandler<CoreWindow^, VisibilityChangedEventArgs^>(this, &App::OnVisibilityChanged);
+
+    window->Closed += 
+        ref new TypedEventHandler<CoreWindow^, CoreWindowEventArgs^>(this, &App::OnWindowClosed);
+
+    // The CoreWindow has been created, so EGL can be initialized.
+    this->InitEGL(window);
+}
+
+// Initializes scene resources
+void App::Load(Platform::String^ entryPoint)
+{
+    this->RecreateRenderer();
+}
+
+void App::RecreateRenderer()
+{
+
+}
+
+void App::Draw(int width, int height)
+{
+    glClearColor(1, 0, 0, 1);
+    glClear(GL_COLOR_BUFFER_BIT);
+}
+
+// This method is called after the window becomes active.
+void App::Run()
+{
+    while (!m_window_closed)
+    {
+        if (m_window_visible)
+        {
+            CoreWindow::GetForCurrentThread()->Dispatcher->ProcessEvents(CoreProcessEventsOption::ProcessAllIfPresent);
+
+            EGLint window_width = 0;
+            EGLint window_height = 0;
+            eglQuerySurface(m_display, m_surface, EGL_WIDTH, &window_width);
+            eglQuerySurface(m_display, m_surface, EGL_HEIGHT, &window_height);
+            
+            this->Draw(window_width, window_height);
+
+            // The call to eglSwapBuffers might not be successful (e.g. due to Device Lost)
+            // If the call fails, then we must reinitialize EGL and the GL resources.
+            if (eglSwapBuffers(m_display, m_surface) != GL_TRUE)
+            {
+                this->DoneEGL();
+
+                this->InitEGL(CoreWindow::GetForCurrentThread());
+                this->RecreateRenderer();
+            }
+        }
+        else
+        {
+            CoreWindow::GetForCurrentThread()->Dispatcher->ProcessEvents(CoreProcessEventsOption::ProcessOneAndAllPending);
+        }
+    }
+
+    this->DoneEGL();
+}
+
+// Terminate events do not cause Uninitialize to be called. It will be called if your IFrameworkView
+// class is torn down while the app is in the foreground.
+void App::Uninitialize()
+{
+}
+
+// Application lifecycle event handler.
+void App::OnActivated(CoreApplicationView^ applicationView, IActivatedEventArgs^ args)
+{
+    // Run() won't start until the CoreWindow is activated.
+    CoreWindow::GetForCurrentThread()->Activate();
+}
+
+// Window event handlers.
+void App::OnVisibilityChanged(CoreWindow^ sender, VisibilityChangedEventArgs^ args)
+{
+    m_window_visible = args->Visible;
+}
+
+void App::OnWindowClosed(CoreWindow^ sender, CoreWindowEventArgs^ args)
+{
+    m_window_closed = true;
+}
+
+void App::InitEGL(CoreWindow^ window)
+{
+    const EGLint config_attribs[] = 
+    {
+        EGL_RED_SIZE, 8,
+        EGL_GREEN_SIZE, 8,
+        EGL_BLUE_SIZE, 8,
+        EGL_DEPTH_SIZE, 24,
+        EGL_NONE
+    };
+
+    const EGLint context_attribs[] = 
+    { 
+        EGL_CONTEXT_CLIENT_VERSION, 2, 
+        EGL_NONE
+    };
+
+    const EGLint surface_attribs[] =
+    {
+        // EGL_ANGLE_SURFACE_RENDER_TO_BACK_BUFFER is part of the same optimization as EGL_ANGLE_DISPLAY_ALLOW_RENDER_TO_BACK_BUFFER (see above).
+        // If you have compilation issues with it then please update your Visual Studio templates.
+        EGL_ANGLE_SURFACE_RENDER_TO_BACK_BUFFER, EGL_TRUE,
+        EGL_NONE
+    };
+
+    const EGLint default_display_attribs[] =
+    {
+        // These are the default display attributes, used to request ANGLE's D3D11 renderer.
+        // eglInitialize will only succeed with these attributes if the hardware supports D3D11 Feature Level 10_0+.
+        EGL_PLATFORM_ANGLE_TYPE_ANGLE, EGL_PLATFORM_ANGLE_TYPE_D3D11_ANGLE,
+
+        // EGL_ANGLE_DISPLAY_ALLOW_RENDER_TO_BACK_BUFFER is an optimization that can have large performance benefits on mobile devices.
+        // Its syntax is subject to change, though. Please update your Visual Studio templates if you experience compilation issues with it.
+        EGL_ANGLE_DISPLAY_ALLOW_RENDER_TO_BACK_BUFFER, EGL_TRUE,
+        
+        // EGL_PLATFORM_ANGLE_ENABLE_AUTOMATIC_TRIM_ANGLE is an option that enables ANGLE to automatically call 
+        // the IDXGIDevice3::Trim method on behalf of the application when it gets suspended. 
+        // Calling IDXGIDevice3::Trim when an application is suspended is a Windows Store application certification requirement.
+        EGL_PLATFORM_ANGLE_ENABLE_AUTOMATIC_TRIM_ANGLE, EGL_TRUE,
+        EGL_NONE,
+    };
+    
+    const EGLint level_9_3_display_attribs[] =
+    {
+        // These can be used to request ANGLE's D3D11 renderer, with D3D11 Feature Level 9_3.
+        // These attributes are used if the call to eglInitialize fails with the default display attributes.
+        EGL_PLATFORM_ANGLE_TYPE_ANGLE, EGL_PLATFORM_ANGLE_TYPE_D3D11_ANGLE,
+        EGL_PLATFORM_ANGLE_MAX_VERSION_MAJOR_ANGLE, 9,
+        EGL_PLATFORM_ANGLE_MAX_VERSION_MINOR_ANGLE, 3,
+        EGL_ANGLE_DISPLAY_ALLOW_RENDER_TO_BACK_BUFFER, EGL_TRUE,
+        EGL_PLATFORM_ANGLE_ENABLE_AUTOMATIC_TRIM_ANGLE, EGL_TRUE,
+        EGL_NONE,
+    };
+
+    const EGLint warp_display_attribs[] =
+    {
+        // These attributes can be used to request D3D11 WARP.
+        // They are used if eglInitialize fails with both the default display attributes and the 9_3 display attributes.
+        EGL_PLATFORM_ANGLE_TYPE_ANGLE, EGL_PLATFORM_ANGLE_TYPE_D3D11_ANGLE,
+        EGL_PLATFORM_ANGLE_DEVICE_TYPE_ANGLE, EGL_PLATFORM_ANGLE_DEVICE_TYPE_WARP_ANGLE,
+        EGL_ANGLE_DISPLAY_ALLOW_RENDER_TO_BACK_BUFFER, EGL_TRUE,
+        EGL_PLATFORM_ANGLE_ENABLE_AUTOMATIC_TRIM_ANGLE, EGL_TRUE,
+        EGL_NONE,
+    };
+    
+    EGLConfig config = nullptr;
+
+    // eglGetPlatformDisplayEXT is an alternative to eglGetDisplay. It allows us to pass in display attributes, used to configure D3D11.
+    PFNEGLGETPLATFORMDISPLAYEXTPROC eglGetPlatformDisplayEXT = reinterpret_cast<PFNEGLGETPLATFORMDISPLAYEXTPROC>(eglGetProcAddress("eglGetPlatformDisplayEXT"));
+    if (!eglGetPlatformDisplayEXT)
+    {
+        throw Exception::CreateException(E_FAIL, L"Failed to get function eglGetPlatformDisplayEXT");
+    }
+
+    //
+    // To initialize the display, we make three sets of calls to eglGetPlatformDisplayEXT and eglInitialize, with varying 
+    // parameters passed to eglGetPlatformDisplayEXT:
+    // 1) The first calls uses "defaultDisplayAttributes" as a parameter. This corresponds to D3D11 Feature Level 10_0+.
+    // 2) If eglInitialize fails for step 1 (e.g. because 10_0+ isn't supported by the default GPU), then we try again 
+    //    using "fl9_3DisplayAttributes". This corresponds to D3D11 Feature Level 9_3.
+    // 3) If eglInitialize fails for step 2 (e.g. because 9_3+ isn't supported by the default GPU), then we try again 
+    //    using "warpDisplayAttributes".  This corresponds to D3D11 Feature Level 11_0 on WARP, a D3D11 software rasterizer.
+    //
+    
+    // This tries to initialize EGL to D3D11 Feature Level 10_0+. See above comment for details.
+    m_display = eglGetPlatformDisplayEXT(EGL_PLATFORM_ANGLE_ANGLE, EGL_DEFAULT_DISPLAY, default_display_attribs);
+    if (m_display == EGL_NO_DISPLAY)
+    {
+        throw Exception::CreateException(E_FAIL, L"Failed to get EGL display");
+    }
+
+    if (eglInitialize(m_display, NULL, NULL) == EGL_FALSE)
+    {
+        // This tries to initialize EGL to D3D11 Feature Level 9_3, if 10_0+ is unavailable (e.g. on some mobile devices).
+        m_display = eglGetPlatformDisplayEXT(EGL_PLATFORM_ANGLE_ANGLE, EGL_DEFAULT_DISPLAY, level_9_3_display_attribs);
+        if (m_display == EGL_NO_DISPLAY)
+        {
+            throw Exception::CreateException(E_FAIL, L"Failed to get EGL display");
+        }
+
+        if (eglInitialize(m_display, NULL, NULL) == EGL_FALSE)
+        {
+            // This initializes EGL to D3D11 Feature Level 11_0 on WARP, if 9_3+ is unavailable on the default GPU.
+            m_display = eglGetPlatformDisplayEXT(EGL_PLATFORM_ANGLE_ANGLE, EGL_DEFAULT_DISPLAY, warp_display_attribs);
+            if (m_display == EGL_NO_DISPLAY)
+            {
+                throw Exception::CreateException(E_FAIL, L"Failed to get EGL display");
+            }
+
+            if (eglInitialize(m_display, NULL, NULL) == EGL_FALSE)
+            {
+                // If all of the calls to eglInitialize returned EGL_FALSE then an error has occurred.
+                throw Exception::CreateException(E_FAIL, L"Failed to initialize EGL");
+            }
+        }
+    }
+
+    EGLint config_count = 0;
+    if ((eglChooseConfig(m_display, config_attribs, &config, 1, &config_count) == EGL_FALSE) || (config_count == 0))
+    {
+        throw Exception::CreateException(E_FAIL, L"Failed to choose first EGLConfig");
+    }
+
+    // Create a PropertySet and initialize with the EGLNativeWindowType.
+    PropertySet^ surface_properties = ref new PropertySet();
+    surface_properties->Insert(ref new String(EGLNativeWindowTypeProperty), window);
+
+    // You can configure the surface to render at a lower resolution and be scaled up to
+    // the full window size. This scaling is often free on mobile hardware.
+    //
+    // One way to configure the SwapChainPanel is to specify precisely which resolution it should render at.
+    // Size customRenderSurfaceSize = Size(800, 600);
+    // surfaceCreationProperties->Insert(ref new String(EGLRenderSurfaceSizeProperty), PropertyValue::CreateSize(customRenderSurfaceSize));
+    //
+    // Another way is to tell the SwapChainPanel to render at a certain scale factor compared to its size.
+    // e.g. if the SwapChainPanel is 1920x1280 then setting a factor of 0.5f will make the app render at 960x640
+    // float customResolutionScale = 0.5f;
+    // surfaceCreationProperties->Insert(ref new String(EGLRenderResolutionScaleProperty), PropertyValue::CreateSingle(customResolutionScale));
+
+    m_surface = eglCreateWindowSurface(m_display, config, reinterpret_cast<IInspectable*>(surface_properties), surface_attribs);
+    if (m_surface == EGL_NO_SURFACE)
+    {
+        throw Exception::CreateException(E_FAIL, L"Failed to create EGL fullscreen surface");
+    }
+
+    m_context = eglCreateContext(m_display, config, EGL_NO_CONTEXT, context_attribs);
+    if (m_context == EGL_NO_CONTEXT)
+    {
+        throw Exception::CreateException(E_FAIL, L"Failed to create EGL context");
+    }
+
+    if (eglMakeCurrent(m_display, m_surface, m_surface, m_context) == EGL_FALSE)
+    {
+        throw Exception::CreateException(E_FAIL, L"Failed to make fullscreen EGLSurface current");
+    }
+}
+
+void App::DoneEGL()
+{
+    if (m_display != EGL_NO_DISPLAY && m_surface != EGL_NO_SURFACE)
+    {
+        eglDestroySurface(m_display, m_surface);
+        m_surface = EGL_NO_SURFACE;
+    }
+
+    if (m_display != EGL_NO_DISPLAY && m_context != EGL_NO_CONTEXT)
+    {
+        eglDestroyContext(m_display, m_context);
+        m_context = EGL_NO_CONTEXT;
+    }
+
+    if (m_display != EGL_NO_DISPLAY)
+    {
+        eglTerminate(m_display);
+        m_display = EGL_NO_DISPLAY;
+    }
+}
---
+++ app/project/uwp/App.h
@@ -0,0 +1,39 @@
+﻿#pragma once
+
+#include <wrl.h>
+#include <EGL/egl.h>
+
+namespace app
+{
+    ref class App sealed : public Windows::ApplicationModel::Core::IFrameworkView
+    {
+    public:
+        App();
+
+        // IFrameworkView Methods.
+        virtual void Initialize(Windows::ApplicationModel::Core::CoreApplicationView^ applicationView);
+        virtual void SetWindow(Windows::UI::Core::CoreWindow^ window);
+        virtual void Load(Platform::String^ entryPoint);
+        virtual void Run();
+        virtual void Uninitialize();
+
+    private:
+        void InitEGL(Windows::UI::Core::CoreWindow^ window);
+        void DoneEGL();
+        void RecreateRenderer();
+        void Draw(int width, int height);
+
+        // Application lifecycle event handlers.
+        void OnActivated(Windows::ApplicationModel::Core::CoreApplicationView^ applicationView, Windows::ApplicationModel::Activation::IActivatedEventArgs^ args);
+
+        // Window event handlers.
+        void OnVisibilityChanged(Windows::UI::Core::CoreWindow^ sender, Windows::UI::Core::VisibilityChangedEventArgs^ args);
+        void OnWindowClosed(Windows::UI::Core::CoreWindow^ sender, Windows::UI::Core::CoreWindowEventArgs^ args);
+
+        bool m_window_closed;
+        bool m_window_visible;
+        EGLDisplay m_display;
+        EGLContext m_context;
+        EGLSurface m_surface;
+    };
+}
