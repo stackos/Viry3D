825abba64b82ca783852c54fd3ddf7660382f453
stackos <stackos@qq.com>
2018-10-09 01:49:49 +0800

    improve ui image.

--- app/src/DemoUI.h
+++ app/src/DemoUI.h
@@ -32,50 +32,39 @@ namespace Viry3D
             auto canvas = RefMake<CanvasRenderer>();
             m_ui_camera->AddRenderer(canvas);
 
-            auto texture0 = Texture::LoadTexture2DFromFile(Application::Instance()->GetDataPath() + "/texture/checkflag.png", FilterMode::Linear, SamplerAddressMode::ClampToEdge, false);
-            auto texture1 = Texture::LoadTexture2DFromFile(Application::Instance()->GetDataPath() + "/texture/ui/1.png", FilterMode::Linear, SamplerAddressMode::ClampToEdge, false);
-            auto texture2 = Texture::LoadTexture2DFromFile(Application::Instance()->GetDataPath() + "/texture/ui/2.png", FilterMode::Linear, SamplerAddressMode::ClampToEdge, false);
-            auto texture3 = Texture::LoadTexture2DFromFile(Application::Instance()->GetDataPath() + "/texture/ui/3.png", FilterMode::Linear, SamplerAddressMode::ClampToEdge, false);
-            auto texture4 = Texture::LoadTexture2DFromFile(Application::Instance()->GetDataPath() + "/texture/ui/4.png", FilterMode::Linear, SamplerAddressMode::ClampToEdge, false);
-            auto texture5 = Texture::LoadTexture2DFromFile(Application::Instance()->GetDataPath() + "/texture/ui/5.png", FilterMode::Linear, SamplerAddressMode::ClampToEdge, false);
+            auto texture = Texture::LoadTexture2DFromFile(Application::Instance()->GetDataPath() + "/texture/ui/button.png", FilterMode::Linear, SamplerAddressMode::ClampToEdge, false);
 
-            auto sprite = RefMake<Sprite>();
-            sprite->SetSize(Vector2i(100, 100));
-            sprite->SetTexture(texture0);
+            Color colors[] = {
+                Color(201, 48, 44, 255) / 255.0f,
+                Color(236, 151, 31, 255) / 255.0f,
+                Color(49, 176, 213, 255) / 255.0f,
+                Color(68, 157, 68, 255) / 255.0f,
+                Color(40, 96, 144, 255) / 255.0f,
+                Color(230, 230, 230, 255) / 255.0f,
+            };
 
-            canvas->AddView(sprite);
+            for (int i = 0; i < 6; ++i)
+            {
+                auto button = RefMake<Button>();
+                button->SetSize(Vector2i(165, 68));
+                button->SetOffset(Vector2i(-500 + i * 200, 0));
+                button->SetTexture(texture);
+                button->SetColor(colors[i]);
+                button->GetLabel()->SetText("button");
+                if (i == 5)
+                {
+                    button->GetLabel()->SetColor(Color::Black());
+                }
+                else
+                {
+                    button->GetLabel()->SetColor(Color::White());
+                }
+                button->SetOnClick([]() {
+                    Log("click button");
+                });
 
-            sprite = RefMake<Sprite>();
-            sprite->SetSize(Vector2i(100, 100));
-            sprite->SetOffset(Vector2i(400, 100));
-            sprite->SetTexture(texture1);
-
-            canvas->AddView(sprite);
-
-            sprite = RefMake<Sprite>();
-            sprite->SetSize(Vector2i(100, 100));
-            sprite->SetOffset(Vector2i(-400, -100));
-            sprite->SetTexture(texture2);
-
-            canvas->AddView(sprite);
-
-            sprite = RefMake<Sprite>();
-            sprite->SetSize(Vector2i(100, 100));
-            sprite->SetOffset(Vector2i(400, -100));
-            sprite->SetTexture(texture5);
-            sprite->SetLocalRotation(Quaternion::Euler(Vector3(0, 0, 45)));
-
-            canvas->AddView(sprite);
-
-            auto button = RefMake<Button>();
-            button->SetSize(Vector2i(100, 100));
-            button->SetOffset(Vector2i(-400, 100));
-            button->GetLabel()->SetText("button");
-            button->SetOnClick([]() {
-                Log("click button");
-            });
-
-            canvas->AddView(button);
+                canvas->AddView(button);
+            }
         }
 
         virtual void Init()
