e72ddfd7c17680450c8e0917fcdd936e7233b288
stackos <stackos@qq.com>
2019-01-02 00:12:43 +0800

    update license date to 2019.

--- app/project/uwp/Main.cpp
+++ app/project/uwp/Main.cpp
@@ -1,6 +1,6 @@
 ﻿/*
 * Viry3D
-* Copyright 2014-2018 by Stack - stackos@qq.com
+* Copyright 2014-2019 by Stack - stackos@qq.com
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
