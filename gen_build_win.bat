5f6122458127697b1d5602f9f87389bebdb51603
Stack <stackos@qq.com>
2019-01-07 20:15:12 +0800

    add source groups.

--- gen_build_win.bat
+++ gen_build_win.bat
@@ -1,12 +1,8 @@
 @echo off
-if exist build/win_x64 (
-    cd build/win_x64
-) else (
-    mkdir build
-    cd build
-    mkdir win_x64
-    cd win_x64
+if not exist build\win_x64 (
+    mkdir build\win_x64
 )
-cmake ../../ -G "Visual Studio 15 2017 Win64" -DTarget=Windows -DArch=x64
-cd ../../
+cd build\win_x64
+cmake ..\..\ -G "Visual Studio 15 2017 Win64" -DTarget=Windows -DArch=x64
+cd ..\..\
 pause
