210bb9c33a026a8e65b8e4630e9b726c0b7b1e3f
Stack <stackos@qq.com>
2019-04-07 20:38:51 +0800

    add mac flag key input.

--- app/project/mac/VkView.mm
+++ app/project/mac/VkView.mm
@@ -21,6 +21,9 @@
 
 using namespace Viry3D;
 
+extern bool g_key_down[(int) KeyCode::COUNT];
+extern bool g_key[(int) KeyCode::COUNT];
+extern bool g_key_up[(int) KeyCode::COUNT];
 extern float g_mouse_scroll_wheel;
 
 @implementation View
@@ -45,45 +48,85 @@ extern float g_mouse_scroll_wheel;
     return layer;
 }
 
--(BOOL)acceptsFirstResponder {
+- (BOOL)acceptsFirstResponder {
     return YES;
 }
 
-- (void)keyUp:(NSEvent*)event {
-    NSLog(@"keyUp");
+- (void)onKeyDown:(int)key {
+    if (key >= 0) {
+        if (!g_key[key]) {
+            g_key_down[key] = true;
+            g_key[key] = true;
+        }
+    }
+}
+
+- (void)onKeyUp:(int)key {
+    if (key >= 0) {
+        g_key_up[key] = true;
+        g_key[key] = false;
+    }
 }
 
 - (void)keyDown:(NSEvent*)event {
-    NSLog(@"keyDown");
+    
 }
 
--(void)flagsChanged:(NSEvent*)event {
-    NSLog(@"flagsChanged");
+- (void)keyUp:(NSEvent*)event {
+    
 }
 
--(void)scrollWheel:(NSEvent*)event {
-    if (event.type == NSEventTypeScrollWheel) {
-        float wheel_dy = 0.0;
-        
-#if MAC_OS_X_VERSION_MAX_ALLOWED >= 1070
-        if (floor(NSAppKitVersionNumber) > NSAppKitVersionNumber10_6)
-        {
-            wheel_dy = [event scrollingDeltaY];
-            if ([event hasPreciseScrollingDeltas])
-            {
-                wheel_dy *= 0.1f;
-            }
-        }
-        else
-#endif
-        {
-            wheel_dy = [event deltaY];
-        }
-        
-        if (fabs(wheel_dy) > 0.0f) {
-            g_mouse_scroll_wheel = wheel_dy * 0.1f;
+- (void)flagsChanged:(NSEvent*)event {
+    unsigned int flags = [event modifierFlags] & NSEventModifierFlagDeviceIndependentFlagsMask;
+    
+    bool ctrl = flags & NSEventModifierFlagControl;
+    bool shift = flags & NSEventModifierFlagShift;
+    bool alt = flags & NSEventModifierFlagOption;
+    
+    if (ctrl) {
+        [self onKeyDown:(int) KeyCode::LeftControl];
+    } else {
+        if (g_key[(int) KeyCode::LeftControl]) {
+            [self onKeyUp:(int) KeyCode::LeftControl];
         }
     }
+    
+    if (shift) {
+        [self onKeyDown:(int) KeyCode::LeftShift];
+    } else {
+        if (g_key[(int) KeyCode::LeftShift]) {
+            [self onKeyUp:(int) KeyCode::LeftShift];
+        }
+    }
+    
+    if (alt) {
+        [self onKeyDown:(int) KeyCode::LeftAlt];
+    } else {
+        if (g_key[(int) KeyCode::LeftAlt]) {
+            [self onKeyUp:(int) KeyCode::LeftAlt];
+        }
+    }
+}
+
+- (void)scrollWheel:(NSEvent*)event {
+    float wheel_dy = 0.0;
+    
+#if MAC_OS_X_VERSION_MAX_ALLOWED >= 1070
+    if (floor(NSAppKitVersionNumber) > NSAppKitVersionNumber10_6)
+    {
+        wheel_dy = [event scrollingDeltaY];
+        if ([event hasPreciseScrollingDeltas])
+        {
+            wheel_dy *= 0.1f;
+        }
+    }
+    else
+#endif
+    {
+        wheel_dy = [event deltaY];
+    }
+    
+    g_mouse_scroll_wheel = wheel_dy * 0.1f;
 }
 
 @end
