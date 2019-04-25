2c8c031748a169be669382bf75fa3a25590b13ee
stackos <stackos@qq.com>
2019-04-26 02:56:07 +0800

    apply basic srp.

---
+++ unity/Assets/script/SRP.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Experimental.Rendering;
+
+public class SRP : RenderPipeline
+{
+    public override void Render(ScriptableRenderContext context, Camera[] cameras)
+    {
+        base.Render(context, cameras);
+
+        for (int i = 0; i < cameras.Length; ++i)
+        {
+            Camera camera = cameras[i];
+
+            ScriptableCullingParameters culling_params;
+            if (!CullResults.GetCullingParameters(camera, out culling_params))
+            {
+                continue;
+            }
+            CullResults cull = CullResults.Cull(ref culling_params, context);
+
+            context.SetupCameraProperties(camera);
+            var cmd = new CommandBuffer();
+            cmd.ClearRenderTarget(true, true, Color.black);
+            context.ExecuteCommandBuffer(cmd);
+            cmd.Release();
+
+            var draw = new DrawRendererSettings(camera, new ShaderPassName("Unlit"));
+            draw.sorting.flags = SortFlags.CommonOpaque;
+            var filter = new FilterRenderersSettings(true);
+            filter.renderQueueRange = RenderQueueRange.opaque;
+            context.DrawRenderers(cull.visibleRenderers, ref draw, filter);
+            context.Submit();
+        }
+    }
+}
---
+++ unity/Assets/script/SRPAsset.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine.Experimental.Rendering;
+
+public class SRPAsset : RenderPipelineAsset
+{
+#if UNITY_EDITOR
+    [UnityEditor.MenuItem("Viry3D/Create SRP")]
+    static void CreateSRP()
+    {
+        UnityEditor.AssetDatabase.CreateAsset(CreateInstance<SRPAsset>(), "Assets/SRP.asset");
+    }
+#endif
+
+    protected override IRenderPipeline InternalCreatePipeline()
+    {
+        return new SRP();
+    }
+}
