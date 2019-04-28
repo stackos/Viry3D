2f9a708ea53060a50d4f4c16cde388ddf5a8437b
Stack <stackos@qq.com>
2019-04-28 10:23:03 +0800

    update cmake to 3.14.3 for visual studio 2019.

--- gen_build_win.bat
+++ gen_build_win.bat
@@ -3,6 +3,6 @@ if not exist build\win_x64 (
     mkdir build\win_x64
 )
 cd build\win_x64
-cmake ..\..\ -G "Visual Studio 15 2017 Win64" -DTarget=Windows -DArch=x64
+cmake ..\..\ -G "Visual Studio 16 2019" -A x64 -DTarget=Windows -DArch=x64
 cd ..\..\
 pause
