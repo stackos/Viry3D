eaaeafad7f9d410442414cc8ffb18be69877fe8d
Stack <stackos@qq.com>
2019-06-20 11:29:49 +0800

    add cloth shader.

--- app/bin/Assets/shader/UnityChan/Clothing.lua
+++ app/bin/Assets/shader/UnityChan/Clothing.lua
@@ -1,76 +1,8 @@
-local vs = [[
-#ifndef SKIN_ON
-	#define SKIN_ON 0
-#endif
+local CharaMain = require("CharaMain")
 
-VK_UNIFORM_BINDING(0) uniform PerView
-{
-	mat4 u_view_matrix;
-    mat4 u_projection_matrix;
-};
-VK_UNIFORM_BINDING(1) uniform PerRenderer
-{
-	mat4 u_model_matrix;
-};
-VK_UNIFORM_BINDING(3) uniform PerMaterialVertex
-{
-	vec4 u_texture_scale_offset;
-};
-layout(location = 0) in vec4 i_vertex;
-layout(location = 2) in vec2 i_uv;
-VK_LAYOUT_LOCATION(0) out vec2 v_uv;
+local vs = CharaMain.vs
 
-#if (SKIN_ON == 1)
-	VK_UNIFORM_BINDING(2) uniform PerRendererBones
-	{
-		vec4 u_bones[210];
-	};
-	layout(location = 6) in vec4 i_bone_weights;
-	layout(location = 7) in vec4 i_bone_indices;
-	mat4 skin_mat()
-	{
-		int index_0 = int(i_bone_indices.x);
-		int index_1 = int(i_bone_indices.y);
-		int index_2 = int(i_bone_indices.z);
-		int index_3 = int(i_bone_indices.w);
-		float weights_0 = i_bone_weights.x;
-		float weights_1 = i_bone_weights.y;
-		float weights_2 = i_bone_weights.z;
-		float weights_3 = i_bone_weights.w;
-		mat4 bone_0 = mat4(u_bones[index_0*3], u_bones[index_0*3+1], u_bones[index_0*3+2], vec4(0, 0, 0, 1));
-		mat4 bone_1 = mat4(u_bones[index_1*3], u_bones[index_1*3+1], u_bones[index_1*3+2], vec4(0, 0, 0, 1));
-		mat4 bone_2 = mat4(u_bones[index_2*3], u_bones[index_2*3+1], u_bones[index_2*3+2], vec4(0, 0, 0, 1));
-		mat4 bone_3 = mat4(u_bones[index_3*3], u_bones[index_3*3+1], u_bones[index_3*3+2], vec4(0, 0, 0, 1));
-		return bone_0 * weights_0 + bone_1 * weights_1 + bone_2 * weights_2 + bone_3 * weights_3;
-	}
-#endif
-
-void main()
-{
-#if (SKIN_ON == 1)
-    mat4 model_matrix = skin_mat();
-#else
-    mat4 model_matrix = u_model_matrix;
-#endif
-	gl_Position = i_vertex * model_matrix * u_view_matrix * u_projection_matrix;
-	v_uv = i_uv * u_texture_scale_offset.xy + u_texture_scale_offset.zw;
-
-	vk_convert();
-}
-]]
-
-local fs = [[
-precision highp float;
-VK_SAMPLER_BINDING(0) uniform sampler2D u_texture;
-VK_LAYOUT_LOCATION(0) in vec2 v_uv;
-layout(location = 0) out vec4 o_color;
-void main()
-{
-	vec4 c = texture(u_texture, v_uv);
-	c.a = 1.0;
-	o_color = c;
-}
-]]
+local fs = CharaMain.fs
 
 --[[
     Cull
@@ -97,6 +29,7 @@ local rs = {
     DstBlendMode = Zero,
 	CWrite = On,
     Queue = Geometry,
+	LightMode = Forward,
 }
 
 local pass = {
@@ -115,6 +48,10 @@ local pass = {
                 {
                     name = "u_projection_matrix",
                     size = 64,
+                },
+				{
+                    name = "u_camera_pos",
+                    size = 16,
                 },
 			},
 		},
@@ -148,6 +85,58 @@ local pass = {
                 },
             },
         },
+		{
+            name = "PerMaterialFragment",
+            binding = 4,
+            members = {
+                {
+                    name = "u_color",
+                    size = 16,
+                },
+				{
+                    name = "_ShadowColor",
+                    size = 16,
+                },
+				{
+                    name = "_SpecularPower",
+                    size = 4,
+                },
+				{
+                    name = "padding",
+                    size = 12,
+                },
+            },
+        },
+		{
+            name = "PerLightFragment",
+            binding = 6,
+            members = {
+				{
+                    name = "u_ambient_color",
+                    size = 16,
+                },
+                {
+                    name = "u_light_pos",
+                    size = 16,
+                },
+                {
+                    name = "u_light_color",
+                    size = 16,
+                },
+				{
+                    name = "u_light_atten",
+                    size = 16,
+                },
+				{
+                    name = "u_spot_light_dir",
+                    size = 16,
+                },
+				{
+                    name = "u_shadow_params",
+                    size = 16,
+                },
+            },
+        },
 	},
 	samplers = {
 		{
@@ -157,6 +146,26 @@ local pass = {
 				{
 					name = "u_texture",
 					binding = 0,
+				},
+				{
+					name = "_FalloffSampler",
+					binding = 1,
+				},
+				{
+					name = "_RimLightSampler",
+					binding = 2,
+				},
+				{
+					name = "_SpecularReflectionSampler",
+					binding = 3,
+				},
+				{
+					name = "_EnvMapSampler",
+					binding = 4,
+				},
+				{
+					name = "_NormalMapSampler",
+					binding = 5,
 				},
 			},
 		},
