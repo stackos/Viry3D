fee75a57c89588b75a5188bf7e0e650674cdec1a
stackos <stackos@qq.com>
2019-08-12 11:32:48 +0800

    upgrade unity to 2018.4.6f1.

--- unity/Assets/PostProcessing/Editor/PropertyDrawers/MinDrawer.cs
+++ unity/Assets/PostProcessing/Editor/PropertyDrawers/MinDrawer.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 
 namespace UnityEditor.PostProcessing
 {
-    [CustomPropertyDrawer(typeof(MinAttribute))]
+    [CustomPropertyDrawer(typeof(UnityEngine.PostProcessing.MinAttribute))]
     sealed class MinDrawer : PropertyDrawer
     {
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
-            MinAttribute attribute = (MinAttribute)base.attribute;
+            UnityEngine.PostProcessing.MinAttribute attribute = (UnityEngine.PostProcessing.MinAttribute)base.attribute;
 
             if (property.propertyType == SerializedPropertyType.Integer)
             {
