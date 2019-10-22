fec484be65412afeb455864a29236cc372ff1aa5
stackos <stackos@qq.com>
2019-10-23 03:31:51 +0800

    add PNGCombiner tool.

---
+++ tool/PNGCombiner/main.cpp
@@ -0,0 +1,14 @@
+#define STBI_ONLY_PNG
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+
+int main(int argc, char* argv[])
+{
+    int w, h, c;
+    unsigned char* data = stbi_load("", &w, &h, &c, 0);
+    stbi_write_png("", w, h, c, data, w * c);
+    stbi_image_free(data);
+    return 0;
+}
