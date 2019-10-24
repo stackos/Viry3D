da4c080359859e3375d0fa30621ba019b2684bb1
stackos <stackos@qq.com>
2019-10-25 03:49:03 +0800

    free image data.

--- tool/PNGCombiner/main.cpp
+++ tool/PNGCombiner/main.cpp
@@ -9,5 +9,6 @@ int main(int argc, char* argv[])
     int w, h, c;
     unsigned char* data = stbi_load("r.png", &w, &h, &c, 0);
     stbi_write_png("o.png", w, h, c, data, w * c);
+    stbi_image_free(data);
     return 0;
 }
