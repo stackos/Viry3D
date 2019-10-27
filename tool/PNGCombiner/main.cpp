3a998bc03505c023eb46f9350ca794af656dc31d
stackos <stackos@qq.com>
2019-10-27 23:36:21 +0800

    move stb headers to lib src.

--- tool/PNGCombiner/main.cpp
+++ tool/PNGCombiner/main.cpp
@@ -1,8 +1,8 @@
 #define STBI_ONLY_PNG
 #define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
+#include "../../lib/src/stb/stb_image.h"
 #define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "stb_image_write.h"
+#include "../../lib/src/stb/stb_image_write.h"
 #include <string>
 #include <vector>
 
