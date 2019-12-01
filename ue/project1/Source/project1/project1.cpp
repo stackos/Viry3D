982bd458892dd4723b17762ab6046187c61e4cf5
stackos <stackos@qq.com>
2019-12-01 15:54:02 +0800

    remove test project.

--- ue/project1/Source/project1/project1.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "project1.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, project1, "project1" );
